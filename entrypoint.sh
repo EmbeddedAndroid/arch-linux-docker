ff6aa92e771d58887bf5e132779358837387c879
Tyler Baker <tyler@foundries.io>
2018-11-16 11:00:02 -0800

    maybe
    Signed-off-by: Tyler Baker <tyler@foundries.io>

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,11 @@
 #!/bin/sh -ex
 
+if test -z "$DBUS_SESSION_BUS_ADDRESS" ; then
+	## if not found, launch a new one
+	eval `dbus-launch --sh-syntax`
+	echo "D-Bus per-session daemon address is: $DBUS_SESSION_BUS_ADDRESS"
+fi
+
 sudo /usr/lib/systemd/systemd-udevd --daemon
 sudo udevadm trigger
 pulseaudio &
