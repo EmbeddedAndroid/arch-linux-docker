4830fefe8861ef39ed066a1e08fdeafca90eb111
Tyler Baker <tyler@foundries.io>
2018-11-15 20:39:11 -0800

    restart
    Signed-off-by: Tyler Baker <tyler@foundries.io>

--- run.sh
+++ run.sh
@@ -4,7 +4,8 @@ docker rm arch
 
 ARCH_USER="${ARCH_USER-osf}"
 
-docker run --rm -it --privileged --name arch --net=host --tmpfs /tmp \
+docker run -dit --restart=always --privileged \
+        --name arch --net=host --tmpfs /tmp \
 	-v /etc/machine-id:/etc/machine-id \
 	-v $HOME:/home/$ARCH_USER \
 	-v /run/user/1000/bus:/run/user/1000/bus \
